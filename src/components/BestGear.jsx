c625e2da7b1192d012fe4c16db47374a8a880cfb
Kausalya Subramaniyan <kausi1198@gmail.com>
2024-10-24 22:50:03 +0530

    #123: Layout changes

---
+++ src/components/BestGear.jsx
@@ -0,0 +1,45 @@
+import { css } from "@emotion/react";
+import { mediaQuery } from "../styles/CommonStyles";
+import SideBySideLayout from "./SideBySideLayout";
+import SideBySideLayoutTextContent from "./SideBySideLayoutTextContent";
+
+export default function BestGear() {
+  return (
+    <div css={styles.layoutContainer}>
+      <SideBySideLayout
+        isImgLeft={false}
+        content={
+          <SideBySideLayoutTextContent
+            tag=""
+            description="Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
+            title={
+              <h2>
+                BRINGING YOU THE
+                <span css={styles.textHighlight}> BEST</span> AUDIO GEAR
+              </h2>
+            }
+          />
+        }
+        imgUrls={{
+          mobile: "/images/shared/mobile/image-best-gear.jpg",
+          tablet: "/images/shared/tablet/image-best-gear.jpg",
+          desktop: "/images/shared/desktop/image-best-gear.jpg",
+        }}
+        imgDimension={{ height: "39rem", width: "100%" }}
+      />
+    </div>
+  );
+}
+
+const styles = {
+  layoutContainer: css({
+    height: "38rem",
+    [mediaQuery[0]]: {
+      height: "50rem",
+    },
+  }),
+  textHighlight: css({
+    color: "var(--color-primary)",
+    font: "inherit",
+  }),
+};
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,7 @@
 import { css } from "@emotion/react";
-import { spacing, colors, mediaQuery } from "../styles/CommonStyles";
+import { colors } from "../styles/CommonStyles";
+import BestGear from "./BestGear";
 import Footer from "./Footer";
-import SideBySideLayout from "./SideBySideLayout";
-import SideBySideLayoutTextContent from "./SideBySideLayoutTextContent";
 import Spacer from "./Spacer";
 
 export default function Layout({ heroSection, content }) {
@@ -12,29 +11,7 @@ export default function Layout({ heroSection, content }) {
       <div css={styles.layout}>
         {content}
         <Spacer value="10rem" />
-        <div css={styles.layoutContainer}>
-          <SideBySideLayout
-            isImgLeft={false}
-            content={
-              <SideBySideLayoutTextContent
-                tag=""
-                description="Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
-                title={
-                  <h2>
-                    BRINGING YOU THE
-                    <span css={styles.textHighlight}> BEST</span> AUDIO GEAR
-                  </h2>
-                }
-              />
-            }
-            imgUrls={{
-              mobile: "/images/shared/mobile/image-best-gear.jpg",
-              tablet: "/images/shared/tablet/image-best-gear.jpg",
-              desktop: "/images/shared/desktop/image-best-gear.jpg",
-            }}
-            imgDimension={{ height: "39rem", width: "100%" }}
-          />
-        </div>
+        <BestGear />
       </div>
       <Spacer value="13rem" />
       <Footer />
@@ -45,15 +22,5 @@ export default function Layout({ heroSection, content }) {
 const styles = {
   layout: css({
     margin: "0rem var(--side-spacing)",
-  }),
-  textHighlight: css({
-    color: colors.orange,
-    font: "inherit",
-  }),
-  layoutContainer: css({
-    height: "38rem",
-    [mediaQuery[0]]: {
-      height: "50rem",
-    },
   }),
 };
